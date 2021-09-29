40b252229ffac395468868aead18a4be7799001d
Austin Roberts <austin.roberts@openrelay.xyz>
2021-09-29 12:17:56 -0500

    Add resumption tool
    A command line tool for retrieving and setting resumption tokens in
    current storage database.

---
+++ cmd/resumption/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+  dbpkg "github.com/openrelayxyz/cardinal-storage/db"
+  "github.com/openrelayxyz/cardinal-storage/db/badgerdb"
+  "github.com/openrelayxyz/cardinal-storage/current"
+  log "github.com/inconshreveable/log15"
+  "os"
+)
+
+
+func main() {
+  db, err := badgerdb.New(os.Args[1])
+  if err != nil {
+    log.Error("Error opening badgerdb", "error", err)
+    os.Exit(1)
+  }
+  defer db.Close()
+  if err := db.Update(func(tx dbpkg.Transaction) error {
+    if len(os.Args) == 2 {
+      // Get
+      resumption, err := tx.Get(current.ResumptionDataKey)
+      if err != nil {
+        log.Error("Error getting Resumption token", "error", err)
+        os.Exit(1)
+      }
+      log.Info("Resumption token", "token", string(resumption))
+    } else if len(os.Args) == 3 {
+      // Put
+      if err := tx.Put(current.ResumptionDataKey, []byte(os.Args[2])); err != nil {
+        log.Error("Error setting resumption token", "error", err)
+        os.Exit(1)
+      }
+      log.Info("Set resumption token")
+    } else {
+      log.Error("Invalid arguments", "args", os.Args)
+      os.Exit(1)
+    }
+    return nil
+  }); err != nil {
+    log.Error("Error starting transaction", "error", err)
+    os.Exit(1)
+  }
+}
