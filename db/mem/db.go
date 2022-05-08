8f9acf902025bcb58262c4861eedff2245756de1
Austin Roberts <austin.roberts@rivet.cloud>
2022-05-07 21:34:21 -0500

    gofmt memory package

--- db/mem/db.go
+++ db/mem/db.go
@@ -1,96 +1,102 @@
 package mem
 
 import (
-  "bytes"
-  "sync"
-  "errors"
-  "fmt"
-  "testing"
-  dbpkg "github.com/openrelayxyz/cardinal-storage/db"
-  "github.com/openrelayxyz/cardinal-storage"
-  log "github.com/inconshreveable/log15"
+	"bytes"
+	"errors"
+	"fmt"
+	log "github.com/inconshreveable/log15"
+	"github.com/openrelayxyz/cardinal-storage"
+	dbpkg "github.com/openrelayxyz/cardinal-storage/db"
+	"sync"
+	"testing"
 )
 
 type Database struct {
-  data map[string][]byte
-  locker *sync.RWMutex
-  wlocker *sync.Mutex
-  semaphore chan struct{}
-  t *testing.T
+	data      map[string][]byte
+	locker    *sync.RWMutex
+	wlocker   *sync.Mutex
+	semaphore chan struct{}
+	t         *testing.T
 }
 
 func NewMemoryDatabase(concurrentReaders int) *Database {
-  return &Database{
-    data: make(map[string][]byte),
-    locker: &sync.RWMutex{},
-    wlocker: &sync.Mutex{},
-    semaphore: make(chan struct{}, concurrentReaders),
-  }
+	return &Database{
+		data:      make(map[string][]byte),
+		locker:    &sync.RWMutex{},
+		wlocker:   &sync.Mutex{},
+		semaphore: make(chan struct{}, concurrentReaders),
+	}
 }
 
 func fnWrapper(fn func(tx dbpkg.Transaction) error) func(tx dbpkg.Transaction) error {
-  return func(tx dbpkg.Transaction) error {
-    if err := fn(tx); err != nil { return err }
-    switch txt := tx.(type) {
-    case *fullTransaction:
-      for k, v := range txt.origValues {
-        if !bytes.Equal(txt.returnedValues[k], v) {
-          return fmt.Errorf("Value changed during transaction: %x != %x", v, txt.returnedValues[k])
-        }
-      }
-    case *viewTransaction:
-      for k, v := range txt.origValues {
-        if !bytes.Equal(txt.returnedValues[k], v) {
-          return fmt.Errorf("Value changed during transaction: %x != %x", v, txt.returnedValues[k])
-        }
-      }
-    default:
-    }
-    return nil
-  }
+	return func(tx dbpkg.Transaction) error {
+		if err := fn(tx); err != nil {
+			return err
+		}
+		switch txt := tx.(type) {
+		case *fullTransaction:
+			for k, v := range txt.origValues {
+				if !bytes.Equal(txt.returnedValues[k], v) {
+					return fmt.Errorf("Value changed during transaction: %x != %x", v, txt.returnedValues[k])
+				}
+			}
+		case *viewTransaction:
+			for k, v := range txt.origValues {
+				if !bytes.Equal(txt.returnedValues[k], v) {
+					return fmt.Errorf("Value changed during transaction: %x != %x", v, txt.returnedValues[k])
+				}
+			}
+		default:
+		}
+		return nil
+	}
 }
 
 func (db *Database) View(fn func(tx dbpkg.Transaction) error) error {
-  db.semaphore <- struct{}{}
-  defer func() {<-db.semaphore}()
-  db.locker.RLock()
-  defer db.locker.RUnlock()
-  return fnWrapper(fn)(&viewTransaction{
-    db: db,
-    origValues: make(map[string][]byte),
-    returnedValues: make(map[string][]byte),
-  })
+	db.semaphore <- struct{}{}
+	defer func() { <-db.semaphore }()
+	db.locker.RLock()
+	defer db.locker.RUnlock()
+	return fnWrapper(fn)(&viewTransaction{
+		db:             db,
+		origValues:     make(map[string][]byte),
+		returnedValues: make(map[string][]byte),
+	})
 }
 
 func (db *Database) Update(fn func(tx dbpkg.Transaction) error) error {
-  if fn == nil { return errors.New("No function provided")}
-  db.wlocker.Lock()
-  defer db.wlocker.Unlock()
-  db.semaphore <- struct{}{}
-  defer func() {<-db.semaphore}()
-  tx := &fullTransaction{
-    db: db,
-    changes: make(map[string][]byte),
-    deletes: make(map[string]struct{}),
-    origValues: make(map[string][]byte),
-    returnedValues: make(map[string][]byte),
-  }
-  if err := fnWrapper(fn)(tx); err != nil { return err }
-  db.locker.Lock()
-  defer db.locker.Unlock()
-  return tx.apply(db.data)
+	if fn == nil {
+		return errors.New("No function provided")
+	}
+	db.wlocker.Lock()
+	defer db.wlocker.Unlock()
+	db.semaphore <- struct{}{}
+	defer func() { <-db.semaphore }()
+	tx := &fullTransaction{
+		db:             db,
+		changes:        make(map[string][]byte),
+		deletes:        make(map[string]struct{}),
+		origValues:     make(map[string][]byte),
+		returnedValues: make(map[string][]byte),
+	}
+	if err := fnWrapper(fn)(tx); err != nil {
+		return err
+	}
+	db.locker.Lock()
+	defer db.locker.Unlock()
+	return tx.apply(db.data)
 }
 
 func (db *Database) BatchWriter() dbpkg.BatchWriter {
-  return &writeBatch{
-    &fullTransaction{
-      db: db,
-      changes: make(map[string][]byte),
-      deletes: make(map[string]struct{}),
-      origValues: make(map[string][]byte),
-      returnedValues: make(map[string][]byte),
-    },
-  }
+	return &writeBatch{
+		&fullTransaction{
+			db:             db,
+			changes:        make(map[string][]byte),
+			deletes:        make(map[string]struct{}),
+			origValues:     make(map[string][]byte),
+			returnedValues: make(map[string][]byte),
+		},
+	}
 }
 
 func (db *Database) Close() {}
@@ -100,106 +106,107 @@ func (db *Database) Vacuum() bool {
 }
 
 type viewTransaction struct {
-  db *Database
-  origValues map[string][]byte
-  returnedValues map[string][]byte
+	db             *Database
+	origValues     map[string][]byte
+	returnedValues map[string][]byte
 }
 
 type memIterator struct {
-  tx dbpkg.Transaction
-  keyCh chan []byte
-  key []byte
-  quit chan struct{}
+	tx    dbpkg.Transaction
+	keyCh chan []byte
+	key   []byte
+	quit  chan struct{}
 }
 
 func (tx *viewTransaction) Get(key []byte) ([]byte, error) {
-  if val, ok := tx.db.data[string(key)]; ok {
-    tx.returnedValues[string(key)] = val
-    tx.origValues[string(key)] = make([]byte, len(val))
-    copy(tx.origValues[string(key)][:], val)
-    return val, nil
-  }
-  return []byte{}, storage.ErrNotFound
+	if val, ok := tx.db.data[string(key)]; ok {
+		tx.returnedValues[string(key)] = val
+		tx.origValues[string(key)] = make([]byte, len(val))
+		copy(tx.origValues[string(key)][:], val)
+		return val, nil
+	}
+	return []byte{}, storage.ErrNotFound
 }
 
 func (tx *viewTransaction) ZeroCopyGet(key []byte, fn func([]byte) error) error {
-  val, err := tx.Get(key)
-  if err != nil { return err }
-  return fn(val)
+	val, err := tx.Get(key)
+	if err != nil {
+		return err
+	}
+	return fn(val)
 }
 
 func (tx *viewTransaction) Put([]byte, []byte) error {
-  return storage.ErrWriteToReadOnly
+	return storage.ErrWriteToReadOnly
 }
 
 func (tx *viewTransaction) PutReserve([]byte, int) ([]byte, error) {
-  return []byte{}, storage.ErrWriteToReadOnly
+	return []byte{}, storage.ErrWriteToReadOnly
 }
 
 func (tx *viewTransaction) Delete([]byte) error {
-  return storage.ErrWriteToReadOnly
+	return storage.ErrWriteToReadOnly
 }
 
 func (tx *viewTransaction) Iterator(prefix []byte) dbpkg.Iterator {
-  iter := &memIterator{
-    tx: tx,
-    keyCh: make(chan []byte),
-    quit: make(chan struct{}),
-  }
-  go func() {
-    for k := range tx.db.data {
-      kb := []byte(k)
-      if bytes.HasPrefix(kb, prefix) {
-        select {
-        case iter.keyCh <- kb:
-        case <-iter.quit:
-          close(iter.keyCh)
-          return
-        }
-      }
-    }
-    close(iter.keyCh)
-  }()
-  return iter
+	iter := &memIterator{
+		tx:    tx,
+		keyCh: make(chan []byte),
+		quit:  make(chan struct{}),
+	}
+	go func() {
+		for k := range tx.db.data {
+			kb := []byte(k)
+			if bytes.HasPrefix(kb, prefix) {
+				select {
+				case iter.keyCh <- kb:
+				case <-iter.quit:
+					close(iter.keyCh)
+					return
+				}
+			}
+		}
+		close(iter.keyCh)
+	}()
+	return iter
 }
 
 func (iter *memIterator) Next() bool {
-  var ok bool
-  iter.key, ok = <-iter.keyCh
-  return ok
+	var ok bool
+	iter.key, ok = <-iter.keyCh
+	return ok
 }
 
 func (iter *memIterator) Key() []byte {
-  return iter.key
+	return iter.key
 }
 
 func (iter *memIterator) Value() []byte {
-  k, _ := iter.tx.Get(iter.key)
-  return k
+	k, _ := iter.tx.Get(iter.key)
+	return k
 }
 
 func (iter *memIterator) Close() error {
-  return nil
+	return nil
 }
 
 func (iter *memIterator) Error() error {
-  return nil
+	return nil
 }
 
-
-type fullTransaction struct{
-  db *Database
-  changes map[string][]byte
-  deletes map[string]struct{}
-  origValues map[string][]byte
-  returnedValues map[string][]byte
+type fullTransaction struct {
+	db             *Database
+	changes        map[string][]byte
+	deletes        map[string]struct{}
+	origValues     map[string][]byte
+	returnedValues map[string][]byte
 }
 
 type writeBatch struct {
 	tx *fullTransaction
 }
 
-func (wb *writeBatch) Put(key, value[]byte) error {
+func (wb *writeBatch) Put(key, value []byte) error {
 	return wb.tx.Put(key, value)
 }
 func (wb *writeBatch) PutReserve(key []byte, size int) ([]byte, error) {
@@ -217,80 +224,85 @@ func (wb *writeBatch) Flush() error {
 func (wb *writeBatch) Cancel() {}
 
 func (tx *fullTransaction) ZeroCopyGet(key []byte, fn func([]byte) error) error {
-  val, err := tx.Get(key)
-  if err != nil { return err }
-  return fn(val)
+	val, err := tx.Get(key)
+	if err != nil {
+		return err
+	}
+	return fn(val)
 }
 
-
 func (tx *fullTransaction) Get(key []byte) ([]byte, error) {
-  if _, ok := tx.deletes[string(key)]; ok {
-    return []byte{}, storage.ErrNotFound
-  }
-  if val, ok := tx.changes[string(key)]; ok {
-    tx.returnedValues[string(key)] = val
-    tx.origValues[string(key)] = make([]byte, len(val))
-    copy(tx.origValues[string(key)][:], val)
-    return val, nil
-  }
-  if val, ok := tx.db.data[string(key)]; ok {
-    tx.returnedValues[string(key)] = val
-    tx.origValues[string(key)] = make([]byte, len(val))
-    copy(tx.origValues[string(key)][:], val)
-    return val, nil
-  }
-  return []byte{}, storage.ErrNotFound
+	if _, ok := tx.deletes[string(key)]; ok {
+		return []byte{}, storage.ErrNotFound
+	}
+	if val, ok := tx.changes[string(key)]; ok {
+		tx.returnedValues[string(key)] = val
+		tx.origValues[string(key)] = make([]byte, len(val))
+		copy(tx.origValues[string(key)][:], val)
+		return val, nil
+	}
+	if val, ok := tx.db.data[string(key)]; ok {
+		tx.returnedValues[string(key)] = val
+		tx.origValues[string(key)] = make([]byte, len(val))
+		copy(tx.origValues[string(key)][:], val)
+		return val, nil
+	}
+	return []byte{}, storage.ErrNotFound
 }
 
 func (tx *fullTransaction) Put(key []byte, val []byte) error {
-  delete(tx.deletes, string(key))
-  tx.changes[string(key)] = val
-  return nil
+	delete(tx.deletes, string(key))
+	tx.changes[string(key)] = val
+	return nil
 }
 
 func (tx *fullTransaction) PutReserve(key []byte, n int) ([]byte, error) {
-  delete(tx.deletes, string(key))
-  tx.changes[string(key)] = make([]byte, n)
-  return tx.changes[string(key)], nil
+	delete(tx.deletes, string(key))
+	tx.changes[string(key)] = make([]byte, n)
+	return tx.changes[string(key)], nil
 }
 
 func (tx *fullTransaction) Delete(key []byte) error {
-  delete(tx.changes, string(key))
-  tx.deletes[string(key)] = struct{}{}
-  return nil
+	delete(tx.changes, string(key))
+	tx.deletes[string(key)] = struct{}{}
+	return nil
 }
 
 func (tx *fullTransaction) Iterator(prefix []byte) dbpkg.Iterator {
-  iter := &memIterator{
-    tx: tx,
-    keyCh: make(chan []byte),
-  }
-  go func() {
-    for k := range tx.changes {
-      kb := []byte(k)
-      if bytes.HasPrefix(kb, prefix) {
-        iter.keyCh <- kb
-      }
-    }
-    for k := range tx.db.data {
-      if _, ok := tx.changes[k]; ok { continue }
-      if _, ok := tx.deletes[k]; ok { continue }
-      kb := []byte(k)
-      if bytes.HasPrefix(kb, prefix) {
-        iter.keyCh <- kb
-      }
-    }
-    close(iter.keyCh)
-  }()
-  return iter
+	iter := &memIterator{
+		tx:    tx,
+		keyCh: make(chan []byte),
+	}
+	go func() {
+		for k := range tx.changes {
+			kb := []byte(k)
+			if bytes.HasPrefix(kb, prefix) {
+				iter.keyCh <- kb
+			}
+		}
+		for k := range tx.db.data {
+			if _, ok := tx.changes[k]; ok {
+				continue
+			}
+			if _, ok := tx.deletes[k]; ok {
+				continue
+			}
+			kb := []byte(k)
+			if bytes.HasPrefix(kb, prefix) {
+				iter.keyCh <- kb
+			}
+		}
+		close(iter.keyCh)
+	}()
+	return iter
 }
 
 func (tx *fullTransaction) apply(data map[string][]byte) error {
-  for k := range tx.deletes {
-    delete(data, k)
-  }
-  for k, v := range tx.changes {
-    data[k] = v
-  }
-  return nil
+	for k := range tx.deletes {
+		delete(data, k)
+	}
+	for k, v := range tx.changes {
+		data[k] = v
+	}
+	return nil
 }
--- db/mem/db_test.go
+++ db/mem/db_test.go
@@ -1,92 +1,128 @@
 package mem
 
 import (
-  "errors"
-  "bytes"
-  "testing"
-  dbpkg "github.com/openrelayxyz/cardinal-storage/db"
+	"bytes"
+	"errors"
+	dbpkg "github.com/openrelayxyz/cardinal-storage/db"
+	"testing"
 )
 
 func TestUpdateTx(t *testing.T) {
-  db := NewMemoryDatabase(1024)
-  err := db.Update(func(tx dbpkg.Transaction) error {
-    tx.Put([]byte("Hello"), []byte("World"))
-    val, err := tx.Get([]byte("Hello"))
-    if err != nil { return err }
-    if !bytes.Equal(val, []byte("World")) {
-      return errors.New("Unexpected value")
-    }
-    return nil
-  })
-  if err != nil { t.Fatalf(err.Error()) }
-  err = db.View(func(tx dbpkg.Transaction) error {
-    if err := tx.Put([]byte("Hello"), []byte("World")); err == nil {
-      t.Errorf("Expected error calling Put() inside view tx")
-    }
-    val, err := tx.Get([]byte("Hello"))
-    if err != nil { t.Fatalf(err.Error()) }
-    if !bytes.Equal(val, []byte("World")) {
-      t.Errorf("Unexpected value: %v", string(val))
-    }
-    return nil
-  })
-  if err != nil { t.Fatalf(err.Error()) }
+	db := NewMemoryDatabase(1024)
+	err := db.Update(func(tx dbpkg.Transaction) error {
+		tx.Put([]byte("Hello"), []byte("World"))
+		val, err := tx.Get([]byte("Hello"))
+		if err != nil {
+			return err
+		}
+		if !bytes.Equal(val, []byte("World")) {
+			return errors.New("Unexpected value")
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	err = db.View(func(tx dbpkg.Transaction) error {
+		if err := tx.Put([]byte("Hello"), []byte("World")); err == nil {
+			t.Errorf("Expected error calling Put() inside view tx")
+		}
+		val, err := tx.Get([]byte("Hello"))
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		if !bytes.Equal(val, []byte("World")) {
+			t.Errorf("Unexpected value: %v", string(val))
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
 }
 func TestBw(t *testing.T) {
-  db := NewMemoryDatabase(1024)
-  bw := db.BatchWriter()
-  err := bw.Put([]byte("Hello"), []byte("World"))
-  bw.Flush()
-  if err != nil { t.Fatalf(err.Error()) }
-  err = db.View(func(tx dbpkg.Transaction) error {
-    if err := tx.Put([]byte("Hello"), []byte("World")); err == nil {
-      t.Errorf("Expected error calling Put() inside view tx")
-    }
-    val, err := tx.Get([]byte("Hello"))
-    if err != nil { t.Fatalf(err.Error()) }
-    if !bytes.Equal(val, []byte("World")) {
-      t.Errorf("Unexpected value: %v", string(val))
-    }
-    return nil
-  })
-  if err != nil { t.Fatalf(err.Error()) }
+	db := NewMemoryDatabase(1024)
+	bw := db.BatchWriter()
+	err := bw.Put([]byte("Hello"), []byte("World"))
+	bw.Flush()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	err = db.View(func(tx dbpkg.Transaction) error {
+		if err := tx.Put([]byte("Hello"), []byte("World")); err == nil {
+			t.Errorf("Expected error calling Put() inside view tx")
+		}
+		val, err := tx.Get([]byte("Hello"))
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		if !bytes.Equal(val, []byte("World")) {
+			t.Errorf("Unexpected value: %v", string(val))
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
 }
 func TestAlterGetTx(t *testing.T) {
-  db := NewMemoryDatabase(1024)
-  err := db.Update(func(tx dbpkg.Transaction) error {
-    tx.Put([]byte("Hello"), []byte("World"))
-    val, err := tx.Get([]byte("Hello"))
-    if err != nil { return err }
-    if !bytes.Equal(val, []byte("World")) {
-      return errors.New("Unexpected value")
-    }
-    val[0] = 3
-    return nil
-  })
-  if err == nil { t.Fatalf("Expected failure") }
+	db := NewMemoryDatabase(1024)
+	err := db.Update(func(tx dbpkg.Transaction) error {
+		tx.Put([]byte("Hello"), []byte("World"))
+		val, err := tx.Get([]byte("Hello"))
+		if err != nil {
+			return err
+		}
+		if !bytes.Equal(val, []byte("World")) {
+			return errors.New("Unexpected value")
+		}
+		val[0] = 3
+		return nil
+	})
+	if err == nil {
+		t.Fatalf("Expected failure")
+	}
 }
-func TestIterTx(t *testing.T){
-  db := NewMemoryDatabase(1024)
-  db.Update(func(tx dbpkg.Transaction) error {
-    tx.Put([]byte("Hello"), []byte("World"))
-    iter := tx.Iterator([]byte("H"))
-    if !iter.Next() { t.Errorf("Iterator should have had next item") }
-    if string(iter.Key()) != "Hello" { t.Errorf("Unexpected key")}
-    if string(iter.Value()) != "World" { t.Errorf("Unexpected value")}
-    if iter.Next() { t.Errorf("Iterator should have been exhausted") }
-    return nil
-  })
-  db.View(func(tx dbpkg.Transaction) error {
-    iter := tx.Iterator([]byte("H"))
-    if !iter.Next() { t.Errorf("Iterator should have had next item") }
-    if string(iter.Key()) != "Hello" { t.Errorf("Unexpected key")}
-    if string(iter.Value()) != "World" { t.Errorf("Unexpected value")}
-    if iter.Next() { t.Errorf("Iterator should have been exhausted") }
-    return nil
-  })
-  db.View(func(tx dbpkg.Transaction) error {
-    iter := tx.Iterator([]byte("Q"))
-    if iter.Next() { t.Errorf("Iterator should have been exhausted") }
-    return nil
-  })
+func TestIterTx(t *testing.T) {
+	db := NewMemoryDatabase(1024)
+	db.Update(func(tx dbpkg.Transaction) error {
+		tx.Put([]byte("Hello"), []byte("World"))
+		iter := tx.Iterator([]byte("H"))
+		if !iter.Next() {
+			t.Errorf("Iterator should have had next item")
+		}
+		if string(iter.Key()) != "Hello" {
+			t.Errorf("Unexpected key")
+		}
+		if string(iter.Value()) != "World" {
+			t.Errorf("Unexpected value")
+		}
+		if iter.Next() {
+			t.Errorf("Iterator should have been exhausted")
+		}
+		return nil
+	})
+	db.View(func(tx dbpkg.Transaction) error {
+		iter := tx.Iterator([]byte("H"))
+		if !iter.Next() {
+			t.Errorf("Iterator should have had next item")
+		}
+		if string(iter.Key()) != "Hello" {
+			t.Errorf("Unexpected key")
+		}
+		if string(iter.Value()) != "World" {
+			t.Errorf("Unexpected value")
+		}
+		if iter.Next() {
+			t.Errorf("Iterator should have been exhausted")
+		}
+		return nil
+	})
+	db.View(func(tx dbpkg.Transaction) error {
+		iter := tx.Iterator([]byte("Q"))
+		if iter.Next() {
+			t.Errorf("Iterator should have been exhausted")
+		}
+		return nil
+	})
 }
